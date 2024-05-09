76ceddc7c48578c1a2412bd6f3a6e3a4bbba879a
Brennon <brennonmeireles7@gmail.com>
2024-05-09 14:55:45 -0300

    feat: created page produto

---
+++ src/pages/Produtos/produtos.jsx
@@ -0,0 +1,17 @@
+import NavBarComponents from "../../component/NavBar";
+import Table from "../../component/Table";
+import FooterComponent from "../../component/Footer";
+
+import "./produtos.css"
+
+export default function PageProdutos() {
+    return(
+        <div>
+            <NavBarComponents />
+            <div className="table-container">
+                <Table />
+            </div>
+                <FooterComponent />
+        </div>
+    )
+}
