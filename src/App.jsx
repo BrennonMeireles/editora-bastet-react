786ef6c6ef82078877b6eac92b799c3196887142
valadao14 <contato.valadao14@gmail.com>
2024-05-10 12:00:50 -0300

    feat: created slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Table from './component/Table'
 import NavBarComponents from './component/NavBar'
+import SliderBanner from './component/Sliders'
 import './App.css'
+
 
 function App() {
   return (
     <div>
       <NavBarComponents />
       <Table />
+      <SliderBanner />
     </div>
   )
 }
