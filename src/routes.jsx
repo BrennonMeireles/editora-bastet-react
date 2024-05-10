619c259487462ddcb1b9a620c1d1c43a5da62b2f
Brennon <brennonmeireles7@gmail.com>
2024-05-10 10:22:53 -0300

    feat: added home page to routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Home        from "./pages/Home"
+import Home         from "./pages/Home/home-index";
 import PageProdutos from "./pages/Produtos/produtos";
 
 export default function Rotas() {
     return(
         <BrowserRouter>
             <Routes>
+                <Route path="/"          element={<Home />} />
                 <Route path="/produtos/" element={<PageProdutos />} />
             </Routes>
         </BrowserRouter>
