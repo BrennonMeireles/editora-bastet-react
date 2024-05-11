b8efae2b710cecc589fd68fc28cba106c1512323
Brennon <brennonmeireles7@gmail.com>
2024-05-11 14:10:14 -0300

    feat: added img to  sliderbook

--- src/pages/Home/home-index.jsx
+++ src/pages/Home/home-index.jsx
@@ -1,16 +1,31 @@
 import NavBarComponents    from "../../component/NavBar";
 import FooterComponent     from "../../component/Footer";
 import SliderBookComponent from "../../component/SliderBooks";
+import SliderBanner        from "../../component/Sliders";
 
-import "./home.css"
-import SliderBanner from "../../component/Sliders";
+import capaLivroHerman     from "../../assets/img/capa-livro-herman.svg"
+import capaLivroMorro      from "../../assets/img/capa-livro-morro.svg"
+import capaLivroVidasSecas from "../../assets/img/capa-livro-vida-secas.svg"
+import capaDomCasmurro     from "../../assets/img/capa-DomCasmurro.svg"
+import capaMorte           from "../../assets/img/capa-liev.svg"
+import capaPequenoPrincipe from "../../assets/img/capa-livro-pequeno_principe.svg"
+
+const title = "Lançamentos"
+const images = [
+    capaLivroHerman,
+    capaLivroMorro,
+    capaLivroVidasSecas,
+    capaMorte,
+    capaDomCasmurro,
+    capaPequenoPrincipe
+]
 
 export default function Home() {
     return (
         <div>
             <NavBarComponents />
             <SliderBanner />
-            <SliderBookComponent />
+            <SliderBookComponent title={title} images={images} />
             <FooterComponent />
         </div>
     )
