3feb9720992c651e6f8efe755d72c6f917ccdd32
Brennon <brennonmeireles7@gmail.com>
2024-05-11 09:05:29 -0300

    fix: added sliderbanner component to home page

--- src/pages/Home/home-index.jsx
+++ src/pages/Home/home-index.jsx
@@ -3,11 +3,13 @@ import FooterComponent     from "../../component/Footer";
 import SliderBookComponent from "../../component/SliderBooks";
 
 import "./home.css"
+import SliderBanner from "../../component/Sliders";
 
 export default function Home() {
     return (
         <div>
             <NavBarComponents />
+            <SliderBanner />
             <SliderBookComponent />
             <FooterComponent />
         </div>
