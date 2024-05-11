69bb60534abebc4018df6ed967dffa13272dae47
Brennon <brennonmeireles7@gmail.com>
2024-05-11 14:09:44 -0300

    fix adjusted style to sliderbook

--- src/component/SliderBooks/style.js
+++ src/component/SliderBooks/style.js
@@ -1,30 +1,30 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 export const SliderBook = styled.div`
-    display: flex;
-    flex-direction: column;
-    border: 1px solid;
-    width: 100vw;
-    height: 50px;
-    padding: 50px;
-    margin-bottom: 100px;
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: auto;
+  padding: 50px;
+  margin-bottom: 100px;
 `
 export const Title = styled.h2`
-  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap');
+  @import url("https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap");
 
   font-family: "Inter", sans-serif;
-  font-size: 36px;
+  font-size:  36px;
   font-weight: 300;
 `
 
 export const ImageContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
-  justify-content: center;
+  justify-content: space-between;
+  padding-top: 50px;
 
   img {
-    max-width: 100%;
-    height: auto;
+    width: 176px;
+    height: 260px;
     margin: 10px;
   }
 `
