2f6d1799094aa91483bc5a11051aa94a239d9823
Brennon <brennonmeireles7@gmail.com>
2024-05-17 10:44:01 -0300

    fix: adjustment to the title of carousels that were not appearing

--- src/component/SliderBooks/index.jsx
+++ src/component/SliderBooks/index.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import { Carousel} from 'antd';
-import { SliderBook } from './style';
+import { Carousel } from 'antd';
+import { SliderBook, Title } from './style'; // Certifique-se de importar o componente de estilo e o título, se aplicável
 
-const App = (props) => (
-    <SliderBook>
-        <Carousel arrows infinite={true} vertical={false} slidesToShow={props.images.length} >
-            {props.images.map((image, index) => (
-                <div key={index}>
-                    <img src={image} alt={`Image ${index}`} style={{ width: "176px", height: "260px", margin: "10px" }} />
-                </div>
-            ))}
-        </Carousel>
-    </SliderBook>
-)
+const SliderBookComponent = (props) => (
+    <div>
+        <Title>{props.title}</Title> {/* Renderiza o título */}
+        <SliderBook>
+            <Carousel arrows infinite={true} vertical={false} slidesToShow={props.images.length} >
+                {props.images.map((image, index) => (
+                    <div key={index}>
+                        <img src={image} alt={`Image ${index}`} style={{ width: "176px", height: "260px", margin: "10px" }} />
+                    </div>
+                ))}
+            </Carousel>
+        </SliderBook>
+    </div>
+);
 
-export default App;
+export default SliderBookComponent;
