5a6c15ac3637d212a7430d411f1c7276b2659c6b
Brennon <brennonmeireles7@gmail.com>
2024-05-17 10:51:28 -0300

    feat: added sliders to home page

--- src/pages/Home/home-index.jsx
+++ src/pages/Home/home-index.jsx
@@ -4,6 +4,7 @@ import SliderBookComponent from "../../component/SliderBooks";
 import SliderBanner        from "../../component/SliderBanner";
 import BannerLacamento     from "../../component/BanLancamento";
 
+// imagens dos sliderbook 1 - Lançamentos
 import capaLivroHerman     from "../../assets/img/capa-livro-herman.svg"
 import capaLivroMorro      from "../../assets/img/capa-livro-morro.svg"
 import capaLivroVidasSecas from "../../assets/img/capa-livro-vida-secas.svg"
@@ -21,13 +22,57 @@ const images = [
     capaPequenoPrincipe
 ]
 
+const title2 = "Romance"
+const images2 = [
+    capaLivroHerman,
+    capaLivroMorro,
+    capaLivroVidasSecas,
+    capaMorte,
+    capaDomCasmurro,
+    capaPequenoPrincipe
+]
+
+const title3 = "Clássicos"
+const images3 = [
+    capaLivroHerman,
+    capaLivroMorro,
+    capaLivroVidasSecas,
+    capaMorte,
+    capaDomCasmurro,
+    capaPequenoPrincipe
+]
+
+const title4 = "Fantasia"
+const images4 = [
+    capaLivroHerman,
+    capaLivroMorro,
+    capaLivroVidasSecas,
+    capaMorte,
+    capaDomCasmurro,
+    capaPequenoPrincipe
+]
+
+const title5 = "Mistério / Suspense"
+const images5 = [
+    capaLivroHerman,
+    capaLivroMorro,
+    capaLivroVidasSecas,
+    capaMorte,
+    capaDomCasmurro,
+    capaPequenoPrincipe
+]
+
 export default function Home() {
     return (
         <div>
             <NavBarComponents />
             <SliderBanner />
-            <SliderBookComponent title={title} images={images} />
+            <SliderBookComponent title={title}  images={images} />
             <BannerLacamento />
+            <SliderBookComponent title={title2} images={images2} />
+            <SliderBookComponent title={title3} images={images3} />
+            <SliderBookComponent title={title4} images={images4} />
+            <SliderBookComponent title={title5} images={images5} />
             <FooterComponent />
         </div>
     )
