6f48f1f3c6c16a19d4e0d87ea06aa32127a0c671
Brennon <brennonmeireles7@gmail.com>
2024-05-18 16:15:51 -0300

    fix: remove widht to sliderbook

--- src/component/SliderBooks/style.js
+++ src/component/SliderBooks/style.js
@@ -4,13 +4,15 @@ export const SliderBook = styled.div`
   display: flex;
   flex-direction: column;
   height: auto;
-  width: 100vw;
+  /* width: 100vw; */
   padding: 20px 50px;
 
   .ant-carousel .slick-prev,
   .ant-carousel .slick-next{
     color: #000;
   }
+
+  
 `
 export const Title = styled.h2`
   @import url("https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap");
