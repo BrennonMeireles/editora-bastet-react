753710b272e6bdb3b5dc286e0c3414d80f3a9eb8
Brennon <brennonmeireles7@gmail.com>
2024-05-18 16:20:18 -0300

    fix: added new img backgorund

--- src/component/BanLancamento/index.jsx
+++ src/component/BanLancamento/index.jsx
@@ -2,25 +2,31 @@ import { BanLancamento, TitleBan, Text, TextContainer, BookContainer, Button } f
 
 import capaLivroMorroVentos from "../../assets/img/capa-livro-morro-vu.png"
 
+import backimg from "../../assets/img/banner-lancamento.png"
+import teste from "../../assets/img/teste.png"
+
 export default function BannerLacamento() {
     return (
         <BanLancamento>
-            <TextContainer>
-                <TitleBan>Lançamento</TitleBan>
-                <Text>
-                    Lorem ipsum dolor sit amet consectetur. Amet vitae turpis ante nullam venenatis amet.
-                    Id justo nulla bibendum ultrices suspendisse id eget vitae. Cras proin cursus libero
-                    aliquam maecenas felis imperdiet. Adipiscing cursus non lectus mauris facilisi cursus.<br></br><br></br>
+            <div>
+                <img src={teste} alt="" />
+                <TextContainer>
+                    <TitleBan>Lançamento</TitleBan>
+                    <Text>
+                        Lorem ipsum dolor sit amet consectetur. Amet vitae turpis ante nullam venenatis amet.
+                        Id justo nulla bibendum ultrices suspendisse id eget vitae. Cras proin cursus libero
+                        aliquam maecenas felis imperdiet. Adipiscing cursus non lectus mauris facilisi cursus.<br></br><br></br>
 
-                    Lorem ipsum dolor sit amet consectetur. Amet vitae turpis ante nullam venenatis amet.
-                    Id justo nulla bibendum ultrices suspendisse id eget vitae. Cras proin cursus libero
-                    aliquam maecenas felis imperdiet. Adipiscing cursus non lectus mauris facilisi cursus.
-                </Text>
-                <Button>ver livro</Button>
-            </TextContainer>
-            <BookContainer>
-                <img src={capaLivroMorroVentos} alt="capa do livro morro dos ventos uivantes" />
-            </BookContainer>
+                        Lorem ipsum dolor sit amet consectetur. Amet vitae turpis ante nullam venenatis amet.
+                        Id justo nulla bibendum ultrices suspendisse id eget vitae. Cras proin cursus libero
+                        aliquam maecenas felis imperdiet. Adipiscing cursus non lectus mauris facilisi cursus.
+                    </Text>
+                    <Button>ver livro</Button>
+                </TextContainer>
+                <BookContainer>
+                    <img src={capaLivroMorroVentos} alt="capa do livro morro dos ventos uivantes" />
+                </BookContainer>
+            </div>
         </BanLancamento>
     )
 }
