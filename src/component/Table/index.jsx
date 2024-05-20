cdec64b635920c2cadfdda84f19dc358c900b760
Brennon <brennonmeireles7@gmail.com>
2024-05-20 08:41:21 -0300

    fix: adjusted img preview table

--- src/component/Table/index.jsx
+++ src/component/Table/index.jsx
@@ -1,3 +1,11 @@
+import capaLivroHerman from "../../assets/img/capa-livro-herman.svg"
+import capaLivroMorro from "../../assets/img/capa-livro-morro.svg"
+import capaLivroVidasSecas from "../../assets/img/capa-livro-vida-secas.svg"
+import capa1984 from "../../assets/img/capa-DomCasmurro.svg"
+import capaMorte from "../../assets/img/capa-liev.svg"
+import capaPequenoPrincipe from "../../assets/img/capa-livro-pequeno_principe.svg"
+
+
 import { Tabela } from "./style";
 
 export default function Table() {
@@ -19,7 +27,7 @@ export default function Table() {
                         <td>10</td>
                         <td>189482</td>
                         <td>Emily Brontë</td>
-                        <td><img src="src/assets/img/capa-livro-morro.svg" alt="" /></td>
+                        <td><img src={capaLivroMorro} alt="" /></td>
                         <td>O Morro dos Ventos Uivantes</td>
                         <td><a href="">excluir</a></td>
                     </tr>
@@ -27,7 +35,7 @@ export default function Table() {
                         <td>9</td>
                         <td>189549</td>
                         <td>Graciliano Ramos</td>
-                        <td><img src="src/assets/img/capa-livro-vida-secas.svg" alt="" /></td>
+                        <td><img src={capaLivroVidasSecas} alt="" /></td>
                         <td>Vidas Secas</td>
                         <td><a href="">excluir</a></td>
                     </tr>
@@ -35,9 +43,32 @@ export default function Table() {
                         <td>8</td>
                         <td>189356</td>
                         <td>Herman Melville</td>
-                        <td><img src="src/assets/img/capa-livro-herman.svg" alt="" /></td>
-                        {/* C:\Users\CT Desenvolvimento\Desktop\Nova pasta\editora-bastet-react\src\assets\img */}
+                        <td><img src={capaLivroHerman} alt="" /></td>
                         <td>Bartleby o Escrivão</td>
+                        <td><a href="">excluir</a></td>
+                    </tr>
+                    <tr>
+                        <td>7</td>
+                        <td>199482</td>
+                        <td>George W.</td>
+                        <td><img src={capa1984} alt="" /></td>
+                        <td>1984</td>
+                        <td><a href="">excluir</a></td>
+                    </tr>
+                    <tr>
+                        <td>6</td>
+                        <td>200765</td>
+                        <td>Liev Tolstói</td>
+                        <td><img src={capaMorte} alt="" /></td>
+                        <td>A Morte de John Jlitch </td>
+                        <td><a href="">excluir</a></td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td>987654</td>
+                        <td>Antoine de S.</td>
+                        <td><img src={capaPequenoPrincipe} alt="" /></td>
+                        <td>O Pequeno Príncipe</td>
                         <td><a href="">excluir</a></td>
                     </tr>
                 </tbody>
