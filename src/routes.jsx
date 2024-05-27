0e8dc5ceca76796175637f9d487d83c5bba31967
Brennon <brennonmeireles7@gmail.com>
2024-05-27 17:49:22 -0300

    feat: added route to page verlivros

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home         from "./pages/Home/home-index";
 import PageProdutos from "./pages/Produtos/produtos";
+import PageLivro    from "./pages/VerLivro/paginaLivro"
 
 export default function Rotas() {
     return(
         <BrowserRouter>
             <Routes>
-                <Route exact path = "/"   element={<Home />} />
-                <Route path = "/produtos" element={<PageProdutos />} />
+                <Route path = "/"          element={<Home />} />
+                <Route path = "/produtos"  element={<PageProdutos />} />
+                <Route path = "/ver-livro" element={<PageLivro />} />
             </Routes>
         </BrowserRouter>
     )
