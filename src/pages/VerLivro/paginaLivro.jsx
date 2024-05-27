4a18db28a6b00bc35a121815e7344ad7bb0a5427
Brennon <brennonmeireles7@gmail.com>
2024-05-27 18:03:15 -0300

    feat: added viewbook component to page

--- src/pages/VerLivro/paginaLivro.jsx
+++ src/pages/VerLivro/paginaLivro.jsx
@@ -1,11 +1,12 @@
 import NavBarComponents from "../../component/NavBar";
-import FooterComponent from "../../component/Footer";
+import FooterComponent  from "../../component/Footer";
+import ViewBookC         from "../../component/ViewBook";
 
 export default function PageLivros() {
     return (
         <div>
             <NavBarComponents />
-            
+            <ViewBookC />
             <FooterComponent />
         </div>
 
