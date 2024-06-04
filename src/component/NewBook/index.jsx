272c9ff93ff98eb2e4d662d23988dcde900f71e5
Emerson <emerson0757@gmail.com>
2024-06-04 19:05:29 -0300

    feat/funcao-adicionar-livro

--- src/component/NewBook/index.jsx
+++ src/component/NewBook/index.jsx
@@ -1,8 +1,43 @@
 import { FaRegPaste } from "react-icons/fa6";
-import { ViewBook, BackBtn, Description, Book } from "../ViewBook/style"
+import { ViewBook, Description, Book } from "../ViewBook/style"
+import {useState } from "react";
+import axios from "axios";
 
 
 export default function NewBook() {
+    
+    const [autor, setAutor] = useState('');
+    const [capa, setCapa] = useState('');
+    const [titulo, setTitulo] = useState('');
+    const [genero, setGenero] = useState('');
+    const [isbn, setISBN] = useState('');
+    const [sinopse, setSinopse] = useState('');
+
+
+    const adicionarLivro = () => {
+        try{
+            axios.post("https://api-editora.onrender.com/livro/salvar", {
+                autor: autor,
+                capa: capa,
+                titulo: titulo, 
+                genero: genero,
+                ISBN: isbn,
+                sinopse: sinopse
+            },
+                window.alert("Livro adicionado com sucesso !!!"),
+                setAutor(''),
+                setCapa(''),
+                setTitulo(''),
+                setGenero(''),
+                setISBN(''),
+                setSinopse('')
+            )
+        } catch(error){
+            console.log("Erro ao salvar livro", error)
+        }
+    }
+
+
     return (
         <ViewBook>
             {/* <BackBtn>voltar</BackBtn> */}
@@ -12,33 +47,30 @@ export default function NewBook() {
                     <p>Adicione a URL da imagem em "CAPA"</p>
                 </article>
                 <div>
-                    <button>excluir</button>
-                    <button>atualizar</button>
-                    <button>adicionar</button>
+                    <button>Excluir</button>
+                    <button>Atualizar</button>
+                    <button onClick={adicionarLivro}>Adicionar</button>
                 </div>
             </Book>
             <Description>
                 <ul>
                     <li>
-                        <label htmlFor="id">ID: </label><input        placeholder="Digite aqui..." id="id"></input>
+                        <label htmlFor="autor">Autor: </label><input   placeholder="Digite aqui..." value={autor} onChange={(e) => (setAutor(e.target.value))} ></input>
                     </li>
                     <li>
-                        <label htmlFor="autor">Autor: </label><input   placeholder="Digite aqui..." ></input>
+                        <label htmlFor="capa">Capa: </label><input     placeholder="Digite aqui..." value={capa} onChange={(e) => (setCapa(e.target.value))}></input>
                     </li>
                     <li>
-                        <label htmlFor="capa">Capa: </label><input     placeholder="Digite aqui..." ></input>
+                        <label htmlFor="titulo">Titulo: </label><input placeholder="Digite aqui..." value={titulo} onChange={(e) => (setTitulo(e.target.value))}></input>
                     </li>
                     <li>
-                        <label htmlFor="titulo">Titulo: </label><input placeholder="Digite aqui..." ></input>
+                        <label htmlFor="genero">Gênero: </label><input placeholder="Digite aqui..." value={genero} onChange={(e) => (setGenero(e.target.value))}></input>
                     </li>
                     <li>
-                        <label htmlFor="genero">Gênero: </label><input placeholder="Digite aqui..." ></input>
+                        <label htmlFor="isbn">ISBN: </label><input     placeholder="Digite aqui..." value={isbn} onChange={(e) => (setISBN(e.target.value))}></input>
                     </li>
                     <li>
-                        <label htmlFor="isbn">ISBN: </label><input     placeholder="Digite aqui..." ></input>
-                    </li>
-                    <li>
-                        <label htmlFor="">Sinopse: </label><textarea name="sinopse" id="sinopse" spellCheck="false" placeholder="Digite aqui..." style={{border: "1px solid grey"}}>
+                        <label htmlFor="">Sinopse: </label><textarea name="sinopse" id="sinopse" spellCheck="false" placeholder="Digite aqui..." style={{border: "1px solid grey"}} value={sinopse} onChange={(e) => (setSinopse(e.target.value))}>
                         </textarea>
                     </li>
                 </ul>
